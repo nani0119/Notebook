87fc994dfad9cc325587d0ae0b2645ecf5fd82a7
zhaorj <renjie.0501@163.com>
2019-01-15 22:16:59 +0800

    增加java笔记

---
+++ java/5-初始化与清理.mm
@@ -0,0 +1,166 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1547558763080" ID="ID_1029909307" MODIFIED="1547561652305" TEXT="5-&#x521d;&#x59cb;&#x5316;&#x4e0e;&#x6e05;&#x7406;">
+<node CREATED="1547558810117" ID="ID_1512274610" MODIFIED="1547558821772" POSITION="right" TEXT="&#x65b9;&#x6cd5;&#x91cd;&#x8f7d;">
+<node CREATED="1547558842434" ID="ID_969658205" MODIFIED="1547558917019" TEXT="&#x901a;&#x8fc7;&#x53c2;&#x6570;&#x5217;&#x8868;&#x7684;&#x4e0d;&#x540c;&#x533a;&#x5206;&#x91cd;&#x8f7d;&#x51fd;&#x6570;"/>
+<node CREATED="1547558922515" ID="ID_357513866" MODIFIED="1547558938253" TEXT="&#x8fd4;&#x56de;&#x503c;&#x4e0d;&#x80fd;&#x7528;&#x6765;&#x533a;&#x5206;&#x91cd;&#x8f7d;&#x51fd;&#x6570;"/>
+</node>
+<node CREATED="1547559000387" ID="ID_1501936016" MODIFIED="1547559015739" POSITION="right" TEXT="&#x9ed8;&#x8ba4;&#x6784;&#x9020;&#x5668;">
+<node CREATED="1547559019801" ID="ID_1884683557" MODIFIED="1547559028178" TEXT="&#x6ca1;&#x6709;&#x53c2;&#x6570;"/>
+<node CREATED="1547559030989" ID="ID_1530252005" MODIFIED="1547559146910" TEXT="&#x672a;&#x663e;&#x5f0f;&#x521b;&#x5efa;&#x6784;&#x9020;&#x5668;&#xff0c;&#x7f16;&#x8bd1;&#x5668;&#x9ed8;&#x8ba4;&#x63d0;&#x4f9b;"/>
+</node>
+<node CREATED="1547559193711" ID="ID_734309859" MODIFIED="1547559201325" POSITION="right" TEXT="this&#x6307;&#x9488;">
+<node CREATED="1547559203015" ID="ID_647080971" MODIFIED="1547559239501" TEXT="&#x65b9;&#x6cd5;&#x5185;&#x90e8;&#x4f7f;&#x7528;&#xff0c;&#x8868;&#x793a;&#x8c03;&#x7528;&#x65b9;&#x6cd5;&#x7684;&#x5bf9;&#x8c61;"/>
+<node CREATED="1547559288288" ID="ID_1264575908" MODIFIED="1547559292536" TEXT="&#x7528;&#x5904;">
+<node CREATED="1547559294408" ID="ID_1692961065" MODIFIED="1547559347092" TEXT="&#x65b9;&#x6cd5;&#x5185;&#x5f15;&#x7528;&#x7c7b;&#x7684;&#x5b57;&#x6bb5;"/>
+<node CREATED="1547559347924" ID="ID_74463684" MODIFIED="1547559409508" TEXT="&#x5728;&#x65b9;&#x6cd5;&#x5185;&#x8fd4;&#x56de;&#x8be5;&#x5bf9;&#x8c61;&#xff0c;&#x5b9e;&#x73b0;&#x94fe;&#x5f0f;&#x8c03;&#x7528;"/>
+<node CREATED="1547559489020" ID="ID_504898110" MODIFIED="1547559509014" TEXT="&#x5c06;&#x5bf9;&#x8c61;&#x81ea;&#x8eab;&#x4f20;&#x9012;&#x7ed9;&#x5916;&#x90e8;&#x65b9;&#x6cd5;"/>
+<node CREATED="1547559592562" ID="ID_1728837411" MODIFIED="1547559611476" TEXT="&#x5728;&#x6784;&#x9020;&#x5668;&#x4e2d;&#x8c03;&#x7528;&#x6784;&#x9020;&#x5668;">
+<node CREATED="1547559613091" ID="ID_898923224" MODIFIED="1547559631043" TEXT="this(s)"/>
+<node CREATED="1547559631588" ID="ID_1886320740" MODIFIED="1547559657747" TEXT="this(&quot;hi&quot;,47)"/>
+</node>
+</node>
+</node>
+<node CREATED="1547559741135" ID="ID_1679805260" MODIFIED="1547559811938" POSITION="right" TEXT="finalize&#x7ec8;&#x7ed3;&#x5904;&#x7406;">
+<node CREATED="1547559813547" ID="ID_503704555" MODIFIED="1547559841955" TEXT="&#x6267;&#x884c;&#x201c;&#x5783;&#x573e;&#x56de;&#x6536;&#x201d;&#x88ab;&#x8c03;&#x7528;"/>
+<node CREATED="1547559842846" ID="ID_1136453593" MODIFIED="1547559933455" TEXT="&#x4e0d;&#x540c;&#x4e8e;&#x6790;&#x6784;&#x51fd;&#x6570;&#xff0c;&#x4e0d;&#x80fd;&#x4fdd;&#x8bc1;&#x88ab;&#x8c03;&#x7528;"/>
+<node CREATED="1547559968730" ID="ID_80871488" MODIFIED="1547559989823" TEXT="java&#x865a;&#x62df;&#x673a;&#x56de;&#x6536;&#x6280;&#x672f;">
+<node CREATED="1547560013490" ID="ID_994037383" MODIFIED="1547560031254" TEXT="&#x6807;&#x8bb0;-&#x6e05;&#x626b;"/>
+<node CREATED="1547560031825" ID="ID_1822315380" MODIFIED="1547560037036" TEXT="&#x505c;&#x6b62;-&#x590d;&#x5236;"/>
+</node>
+</node>
+<node CREATED="1547560068206" ID="ID_1904804843" MODIFIED="1547560429273" POSITION="right" TEXT="&#x521d;&#x59cb;&#x5316;">
+<node CREATED="1547560089740" ID="ID_262952141" MODIFIED="1547560093440" TEXT="&#x987a;&#x5e8f;">
+<node CREATED="1547560096907" ID="ID_554170477" MODIFIED="1547560163809" TEXT="static &#x6210;&#x5458;&#xff0c;&#x65e0;&#x8bba;&#x5728;&#x6784;&#x9020;&#x51fd;&#x6570;&#x5185;&#x51fa;&#x73b0;&#x7684;&#x4f4d;&#x7f6e;&#xff0c;&#x53ea;&#x6267;&#x884c;&#x4e00;&#x6b21;"/>
+<node CREATED="1547560151250" ID="ID_198618583" MODIFIED="1547560273414" TEXT="&#x5206;&#x914d;&#x5806;&#x7a7a;&#x95f4;&#xff0c;&#x521d;&#x59cb;&#x5316;&#x4e3a;&#x9ed8;&#x8ba4;&#x503c;0"/>
+<node CREATED="1547560245457" ID="ID_445807757" MODIFIED="1547560361822" TEXT="&#x5b9a;&#x4e49;&#x5b57;&#x6bb5;&#x7684;&#x521d;&#x59cb;&#x5316;&#x52a8;&#x4f5c;"/>
+<node CREATED="1547560275948" ID="ID_826869770" MODIFIED="1547560303767" TEXT="&#x6784;&#x9020;&#x51fd;&#x6570;&#x521d;&#x59cb;&#x5316;"/>
+</node>
+<node CREATED="1547560440868" ID="ID_961173643" MODIFIED="1547560469647" TEXT="&#x975e;static&#x5b9e;&#x4f8b;&#x521d;&#x59cb;&#x5316;">
+<node CREATED="1547560471867" ID="ID_104996335" MODIFIED="1547560679938">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      public class Mugs {
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Mug mug1;
+    </p>
+    <p>
+      Mug mug2;
+    </p>
+    <p>
+      {
+    </p>
+    <p>
+      &#160;&#160;&#160;mug1 = new Mug(1);
+    </p>
+    <p>
+      &#160;&#160;&#160;mug2 = new Mug(2);
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      Mugs()
+    </p>
+    <p>
+      {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;prinf(&quot;Mugs\n&quot;);
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1547560696773" ID="ID_919297810" MODIFIED="1547560706924" TEXT="&#x6570;&#x7ec4;">
+<node CREATED="1547560709120" ID="ID_179700865" MODIFIED="1547560727045" TEXT="int[] a  &#x5b9a;&#x4e49;&#x5f15;&#x7528;"/>
+<node CREATED="1547561002604" ID="ID_921627758" MODIFIED="1547561038652" TEXT="&#x957f;&#x5ea6; a.length"/>
+<node CREATED="1547560733777" ID="ID_1859216968" MODIFIED="1547560736923" TEXT="&#x521d;&#x59cb;&#x5316;">
+<node CREATED="1547560738853" ID="ID_1665797117" MODIFIED="1547560756629" TEXT="int[] a = {1,2,3}"/>
+<node CREATED="1547560757831" ID="ID_1216112776" MODIFIED="1547560873317">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      int[] a =&#160;&#160;new int[3];
+    </p>
+    <p>
+      a[0] = 1;
+    </p>
+    <p>
+      a[1] = 2;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1547560907986" ID="ID_497699975" MODIFIED="1547560973054" TEXT="Integer [] a = new Interger[] {new Integer(1) ,2,3}"/>
+</node>
+</node>
+</node>
+<node CREATED="1547561062534" ID="ID_1082948863" MODIFIED="1547561071484" POSITION="right" TEXT="&#x53ef;&#x53d8;&#x53c2;&#x6570;&#x5217;&#x8868;">
+<node CREATED="1547561116818" ID="ID_170746799" MODIFIED="1547561263410">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      void f&#65288;int required&#65292;String... args&#65289;
+    </p>
+    <p>
+      {
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;for(String s :args)
+    </p>
+    <p>
+      &#160;&#160;{
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;System.out.print(s);&#160;&#160;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;}
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1547561277993" ID="ID_190618029" MODIFIED="1547561291516" POSITION="right" TEXT="&#x679a;&#x4e3e;&#x7c7b;&#x578b;">
+<node CREATED="1547561293855" ID="ID_1826483314" MODIFIED="1547561369895" TEXT="&#x5b9a;&#x4e49;">
+<node CREATED="1547561372104" ID="ID_1267155279" MODIFIED="1547561422006" TEXT="public enum Spiciness {NOT&#xff0c;MILD}"/>
+</node>
+<node CREATED="1547561354247" ID="ID_317402260" MODIFIED="1547561428769" TEXT="&#x521d;&#x59cb;&#x5316;">
+<node CREATED="1547561434068" ID="ID_1813527008" MODIFIED="1547561465299" TEXT="Spiciness s = Spiciness.not"/>
+</node>
+<node CREATED="1547561467683" ID="ID_1857819599" MODIFIED="1547561603528" TEXT="&#x679a;&#x4e3e;&#x6240;&#x6709;&#x503c;">
+<node CREATED="1547561496351" ID="ID_1782527655" MODIFIED="1547561572775" TEXT="Spiciness..values()"/>
+</node>
+<node CREATED="1547561608555" ID="ID_1931803477" MODIFIED="1547561617891" TEXT="&#x5f53;&#x524d;&#x503c;">
+<node CREATED="1547561509242" ID="ID_1811339412" MODIFIED="1547561542371" TEXT="s.ordinal()"/>
+</node>
+</node>
+</node>
+</map>
